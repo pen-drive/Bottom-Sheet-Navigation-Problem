3100fa53138ff223113e66f6ec03844d387ded6c
indicieLiquido <indicieliquido@gmail.com>
2024-03-22 15:49:58 -0300

    commit

--- app/src/main/java/com/example/bottomsheetnavigationproblem/BottomSheet.kt
+++ app/src/main/java/com/example/bottomsheetnavigationproblem/BottomSheet.kt
@@ -1,17 +1,15 @@
 package com.example.bottomsheetnavigationproblem
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.core.provider.FontsContractCompat.Columns
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.currentStateAsState
 import androidx.navigation.NavBackStackEntry
@@ -21,25 +19,22 @@ import com.ramcosta.composedestinations.spec.DestinationStyleBottomSheet
 
 @Destination(style = DestinationStyleBottomSheet::class)
 @Composable
-fun ColumnScope.BottomSheet(
+fun BottomSheet(
     navBackStackEntry: NavBackStackEntry,
 ) {
 
     val lifecycleState = navBackStackEntry.lifecycle.currentStateAsState()
+    StatePrinter(lifecycleState)
 
-    StatePrinter(lifecycleState, "BottomSheet")
-
-
-    Box(modifier = Modifier.height(350.dp).fillMaxWidth(), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier
+        .height(350.dp)
+        .fillMaxWidth(), contentAlignment = Alignment.Center) {
         Text(text = "Bottom Sheet Content")
     }
 
 }
 
-
 @Composable
-fun StatePrinter(lifecycleState: State<Lifecycle.State>, which: String) {
-//    Timber.d("$which : ${lifecyclerState.value.name}")
-
+fun StatePrinter(lifecycleState: State<Lifecycle.State>) {
+    Log.d("BottomSheet", "State: " + lifecycleState.value.name)
 }
-
